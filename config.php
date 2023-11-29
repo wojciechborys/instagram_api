a103c719eca1f5e64b064f8af690e8fbf289de97
wojciechborys <wojciechjeremiborys@gmail.com>
2023-11-29 22:42:03 +0100

    rm config from remote

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    namespace InstagramApp;
-
-    define('INSTAGRAM_ID', '866692818337343');
-    define('INSTAGRAM_SECRET', 'e876940a309c956915a3904947649942');
-    define('INSTAGRAM_REDIRECT_URI', 'https://api.wojciechborys.pl/wp-admin/admin.php/');    
-?>
